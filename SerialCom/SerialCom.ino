50dc7d5975e6ec459ec20c2462b5b7852ca4479d
jzhou-k <jiayizhou22918@gmail.com>
2023-01-29 15:04:38 -0500

    testing serial communication

---
+++ SerialCom/SerialCom.ino
@@ -0,0 +1,62 @@
+#include <Servo.h>
+
+String sentence = "end of sentence";
+float x_angle = 0;
+float y_angle = 0;
+String angle_info = "";
+Servo myservo;
+
+
+float parseXinfo(String data);
+void serialEvent();
+
+void setup() {
+
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  myservo.attach(9);
+
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0)
+  {
+    char data = Serial.read();
+    angle_info += data;
+    Serial.println(data);
+
+
+
+
+    //Serial.println(Serial.available());
+    if (data == '#')
+    {
+      Serial.print("what");
+    }
+
+  }
+
+
+}
+
+void serialEvent()
+{
+
+}
+
+float parseXinfo(String data)
+{
+  // X25Y25#
+  data.remove(data.indexOf("Y")); //removes Y25 since remove() gets rid of everything to the end of the string
+  data.remove(data.indexOf("X"), 1); //removes X
+  return data.toFloat();
+}
+
+double parseYinfo()
+{
+  //lol
+}
---
+++ test.py
@@ -0,0 +1,94 @@
+import serial
+import datetime
+import time
+import threading
+
+arduino = serial.Serial('COM3', 115200, timeout=.1)
+print("Helloworld")
+
+#arduino to python and python interprets data 
+# while True: 
+#     data = arduino.readline()[:-2]
+#     if data:
+#         print(data)
+
+# # wow chat gpt is very useful assistant ?
+
+def alarmFunction():
+    
+    def countTime(stop_event): 
+        #start = datetime.datetime.now()
+        while not stop_event.is_set():
+            #timeElapsed = datetime.datetime.now() - start
+            print(datetime.datetime.now().strftime("%H:%M:%S"))
+            stop_event.wait(1)
+
+    # Get the current time
+    now = datetime.datetime.now()
+    stop_event = threading.Event()
+
+
+    #TO actually enter the alarm time e.g 7:30 
+    #This is the most dumb solution : ^) 
+    #Parse string to hour min then pass into time delta, the day will be incremented automatically 
+    # 'Wed Jun  9 04:26:40 1993'. standard format 
+    alarmH = 13
+    alarmM = 30 
+    nowH = (int)(now.strftime("%H"))
+    nowM = (int)(now.strftime("%M"))
+
+    diffH = alarmH - nowH 
+    diffM = alarmM - nowM 
+
+    elapseH  = 0 
+    elapseM = 0
+    
+    if(diffH < 0 or (diffH == 0 and diffM < 0)): 
+        elapseH = 23 + diffH 
+        elapseM = 60 + diffM
+        print("{}:{}".format(elapseH,elapseM))
+    elif (diffM < 0): 
+        elapseH = diffH - 1
+        elapseM = 60 + diffM
+        print("{}:{}".format(elapseH,elapseM))
+    else:
+        elapseH = diffH 
+        elapseM = diffM 
+        print("{}:{}".format(elapseH,elapseM))
+
+
+
+    # Calculate the alarm time (10 minutes from now)
+    alarm_time = now + datetime.timedelta(hours=elapseH, minutes=elapseM)
+
+    #Start displayin time elapsed thread 
+    t2 = threading.Thread(target=countTime, args=(stop_event, ))
+    t2.start()
+
+    # Sleep until the alarm time
+    time.sleep((alarm_time - now).total_seconds())
+
+    # Alarm goes off
+    print("Wake up!")
+    stop_event.set()
+
+def writeData(data): 
+    time.sleep(2)
+    arduino.write(data.encode())
+    while True: 
+        data = arduino.read().decode()
+        if data: 
+            #data = data.rstrip("#")
+            print(data, end='')   
+
+writeData("x25y25#")
+
+# ***** START ALARM FUNCTION ***** 
+# t1 = threading.Thread(target=alarmFunction)
+# t1.start()
+# t1.join()
+
+#print("GOOD FUCKING MORNING")
+#Python -> Arduino -> Python again 
+
+
