2d7308d7f65113bb4b3b3421bbb76646f163d3c0
jzhou-k <jiayizhou22918@gmail.com>
2023-02-15 20:48:27 -0500

    haarmodel

---
+++ software/main.py
@@ -0,0 +1,25 @@
+
+
+#Alarm function 
+#Face detect function loop 
+#Better aiming calibration 
+#Additional functions 
+#Mouth aiming
+#Head aiming 
+#Camera and turret targeting 
+
+#Embedded and serial communication 
+#Solidworks 
+
+
+#Default is at shoulders top 
+#Finger tracking 
+#whole hand for stop
+#middle finger detection then more shooting 
+#rock sign to aim at mouth 
+
+
+
+
+
+
