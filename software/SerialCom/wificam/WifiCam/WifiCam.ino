87b7bb965ae033a4c1e790ffc4916b079c331429
jzhou-k <jiayizhou22918@gmail.com>
2023-02-28 20:41:35 -0500

    call an ambulance

--- software/SerialCom/wificam/WifiCam/WifiCam.ino
+++ software/SerialCom/wificam/WifiCam/WifiCam.ino
@@ -2,6 +2,28 @@
 #include <WiFi.h>
 #include <Servo.h>
 
+//declarations for oled display
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_SDA 21
+#define OLED_SCL 22
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+String hour = "g";
+String minute = "a";
+String second = "y";
+String alarmHour = "haha";
+String alarmMinute = "I'm dying inside";
+String timeInfo = "";
+bool myAlarm = false;
+
 float x_angle = 90;
 float y_angle = 90;
 float rawx_angle = 90;
@@ -19,6 +41,7 @@ static const int xServoPin = 33;
 static const int triggerServoPin = 12; //to be determined
 
 boolean data_received = false ;
+bool alarm_data_received = false; 
 boolean start_sweep = false;
 
 void calibrateXangle();
@@ -30,6 +53,10 @@ void serialEvent();
 void trigger();
 void initalizeGun();
 void sweep(int y, int xStart, int xEnd);
+void displayTime();
+void parseTimeHour(String data);
+void parseTimeMinute(String data);
+void parseTimeSecond(String data);
 
 
 static const char* WIFI_SSID = "BELL011";
@@ -39,13 +66,60 @@ esp32cam::Resolution initialResolution;
 
 WebServer server(80);
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000
+};
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
   delay(2000);
 
-  Serial.begin(115200);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+   // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_BLACK);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+
+
+  // Invert and restore display, pausing in-between
+  display.invertDisplay(true);
+  delay(1000);
+  display.invertDisplay(false);
+  delay(1000);
+
+ 
   yservo.attach(
     yServoPin,
     Servo::CHANNEL_NOT_ATTACHED,
@@ -114,15 +188,15 @@ void loop()
 {
   server.handleClient();
 
-
   serialEvent();
+  displayTime(); 
   if (data_received)
   {
 
     if (sweepInfo == 1 && !start_sweep)
     {
       sweep(87, 105, 115);
-      start_sweep = true; 
+      start_sweep = true;
     }
     else
     {
@@ -140,6 +214,8 @@ void loop()
 
 
   }
+
+
   triggerInfo = 0;
   //calibrateEvery();
 
@@ -206,6 +282,35 @@ void calibrateYangle()
   yservo.write(90);
 }
 
+
+void displayTime()
+{
+  display.clearDisplay();
+
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  // Not all the characters will fit on the display. This is normal.
+  // Library will draw what it can and the rest will be clipped.
+  display.println(F("TIME:"));
+  display.print(hour + ":");
+  display.print(minute + ":");
+  display.print(second);
+  display.println("");
+
+  display.println(F("AlarmTime:"));
+  display.print(alarmHour + ":");
+  display.print(alarmMinute + ":)");
+
+  display.display();
+  Serial.println("display");
+  delay(10);
+}
+
+
+
 void serialEvent()
 {
   if (Serial.available() > 0)
@@ -213,9 +318,6 @@ void serialEvent()
     char data = Serial.read();
     angle_info += data;
     // Serial.println(data);
-
-
-
 
     //Serial.println(Serial.available());
     if (data == '#')
@@ -227,9 +329,29 @@ void serialEvent()
       sweepInfo = parseSinfo(angle_info);
       data_received = true;
       angle_info = "";
-    } else
+    } 
+    else
     {
       data_received = false;
+    }
+
+    if(data == 'T')
+    {
+      parseTimeHour(timeInfo);
+      parseTimeMinute(timeInfo);
+      parseTimeSecond(timeInfo);
+      timeInfo = "";
+      alarm_data_received = true;
+      if (!myAlarm)
+      {
+        //registers the first time string as alarm time
+        alarmHour = hour;
+        alarmMinute = minute;
+        myAlarm = true;
+      }
+    }else 
+    {
+      alarm_data_received = false; 
     }
 
 
@@ -279,14 +401,6 @@ void trigger()
   triggerServo.write(20);
   delay(100);
 
-  //For alarm mode
-  // for (int i = 0; i < 3; i++)
-  // {
-  //   triggerServo.write(90);
-  //   delay(300);
-  //   triggerServo.write(20);
-  //   delay(300);
-  // }
 }
 
 //pass in angle
@@ -304,6 +418,31 @@ void sweep(int y, int xStart, int xEnd)
 
   delay(1000);
   xservo.write(90);
+}
+
+
+//18:20:11#
+void parseTimeHour(String data)
+{
+  data.remove(data.indexOf(":"));
+  hour = data;
+}
+
+void parseTimeMinute(String data)
+{
+  data.remove(0, data.indexOf(":") + 1); //removes 18:
+  data.remove(data.indexOf(":"));
+  minute = data;
+}
+
+
+void parseTimeSecond(String data)
+{
+  //myString.remove(index, count)
+  data.remove(0, data.indexOf(":") + 1); //removes 18:
+  data.remove(0, data.indexOf(":") + 1); //removes 18:
+  data.remove(data.indexOf("T"));
+  second = data;
 }
 
 
